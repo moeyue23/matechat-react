b79890520b6b2429b1a5b6a4233d6afd8c571efb
真白 <185361175+moeyue23@users.noreply.github.com>
2025-08-09 10:36:27 +0800

    chore: remove unused React import

--- src/utils/chat.ts
+++ src/utils/chat.ts
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import type { InputOptions } from "./backend";
 import type {
   Backend,
